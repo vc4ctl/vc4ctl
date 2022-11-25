ba0d62871791b371ccf0d00c91ed677f3325675c
Andrew Welker <awelker@pepperdash.com>
2022-11-25 16:46:41 -0700

    chore: fix formatting issues

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,53 @@
-mod cli;
 mod api;
+mod cli;
 
-use clap::{Parser};
-use cli::cli::{Cli, Commands, ConfigCommands};
 use api::config;
+use clap::Parser;
+use cli::cli::{Cli, Commands, ConfigCommands};
 
-
-fn main() -> anyhow::Result<()>{
+fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     match &cli.command {
-        Some(Commands::Config(args)) => {
-            match &args.config_commands {
-                Some(ConfigCommands::Add(args)) => {
-                    match config::config::add_server(args.clone()) {
-                        Ok(()) => {
-                            println!("Server {} successfully added", args.name);
-                            return Ok(());}
-                        Err(error) => return Err(anyhow::anyhow!(error))
-                    }                
+        Some(Commands::Config(args)) => match &args.config_commands {
+            Some(ConfigCommands::Add(args)) => match config::config::add_server(args.clone()) {
+                Ok(()) => {
+                    println!("Server {} successfully added", args.name);
+                    return Ok(());
                 }
-                Some(ConfigCommands::Remove(args)) => {
-                    match config::config::remove_server(args) {
-                        Ok(()) => {
-                            println!("Server {} successfully removed", args.name);
-                            return Ok(());
-                        }
-                        Err(error) => return Err(anyhow::anyhow!(error))
-                    }                    
+                Err(error) => return Err(anyhow::anyhow!(error)),
+            },
+            Some(ConfigCommands::Remove(args)) => match config::config::remove_server(args) {
+                Ok(()) => {
+                    println!("Server {} successfully removed", args.name);
+                    return Ok(());
                 }
-                Some(ConfigCommands::Update(args)) => {
-                    match config::config::update_server(args) {
-                        Ok(()) => {
-                            println!("Server {} successfully updated", args.name);
-                            return Ok(());
-                        }
-                        Err(error) => return Err(anyhow::anyhow!(error))
-                    }
+                Err(error) => return Err(anyhow::anyhow!(error)),
+            },
+            Some(ConfigCommands::Update(args)) => match config::config::update_server(args) {
+                Ok(()) => {
+                    println!("Server {} successfully updated", args.name);
+                    return Ok(());
                 }
-                Some(ConfigCommands::Use(args)) => {
-                    match config::config::use_server(args.clone()){
-                        Ok(()) => {
-                            println!("Using Server {}", args.name);
-                            return Ok(());
-                        }
-                        Err(error) => return Err(anyhow::anyhow!(error))
-                    }
+                Err(error) => return Err(anyhow::anyhow!(error)),
+            },
+            Some(ConfigCommands::Use(args)) => match config::config::use_server(args.clone()) {
+                Ok(()) => {
+                    println!("Using Server {}", args.name);
+                    return Ok(());
                 }
-                Some(ConfigCommands::GetServers(_args)) => {
-                    match config::config::get_servers() {
-                        Ok(()) => {                            
-                            return Ok(());
-                        }
-                        Err(error) => return Err(anyhow::anyhow!(error))
-                    }
+                Err(error) => return Err(anyhow::anyhow!(error)),
+            },
+            Some(ConfigCommands::GetServers(_args)) => match config::config::get_servers() {
+                Ok(()) => {
+                    return Ok(());
                 }
-                None => {
-                    return Err(anyhow::anyhow!("No valid arguments"));
-                }
+                Err(error) => return Err(anyhow::anyhow!(error)),
+            },
+            None => {
+                return Err(anyhow::anyhow!("No valid arguments"));
             }
-        }
+        },
         None => {
             println!("No valid arguments")
         }

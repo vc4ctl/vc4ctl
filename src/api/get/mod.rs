2ff1e84c3c1e13e7a3dc2f602f6ea3ceb6abd047
Andrew Welker <awelker@pepperdash.com>
2022-11-26 22:35:13 -0700

    feat: add get programs

--- src/api/get/mod.rs
+++ src/api/get/mod.rs
@@ -31,6 +31,10 @@ pub struct Programs {
     #[serde(default)]
     #[serde(rename = "ProgramInstanceLibrary")]
     pub program_instance_library: Option<HashMap<String, ProgramInstance>>,
+    
+    #[serde(default)]
+    #[serde(rename="ProgramLibrary")]
+    pub program_library: Option<HashMap<u32, Program>>
 }
 
 #[derive(Deserialize, Debug, Tabled, Default, Clone)]
@@ -68,6 +72,48 @@ pub struct ProgramInstance {
     debugging_enabled: bool,
 }
 
+#[derive(Deserialize, Debug, Tabled, Default, Clone)]
+pub struct Program {    
+    #[serde(rename = "ProgramId")]
+    program_id: u64,
+    #[serde(rename = "FriendlyName")]
+    friendly_name: String,
+    #[serde(rename="Notes")]
+    notes: String,
+    #[serde(rename="AppFile")]
+    app_file:String,
+    #[serde(rename="AppFileTS")]
+    app_file_ts:String,
+    #[serde(rename="MobilityFile")]
+    mobility_file:String,
+    #[serde(rename="MobilityFileTS")]
+    mobility_file_ts:String,
+    #[serde(rename="WebxPanelFile")]
+    web_xpanel_file:String,
+    #[serde(rename="WebxPanelFileTS")]
+    web_xpanel_file_ts:String,
+    #[serde(rename="ProjectFile")]
+    project_file:String,
+    #[serde(rename="ProjectFileTS")]
+    project_file_ts:String,
+    #[serde(rename="CwsFile")]
+    cws_file:String,
+    #[serde(rename="CwsFileTS")]
+    cws_file_ts:String,
+    #[serde(rename="ProgramType")]
+    program_type:String,
+    #[serde(rename="ProgramName")]
+    program_name:String,
+    #[serde(rename="CompileDateTime")]
+    compile_date_time:String,
+    #[serde(rename="CresDBVersion")]
+    cres_db_version:String,
+    #[serde(rename="DeviceDBVersion")]
+    device_db_version:String,
+    #[serde(rename="IncludeDatVersion")]
+    include_dat_version: String,
+}
+
 pub fn get_resource(args: &GetArgs) -> Result<()> {
     let server =
         config::get_current_server().with_context(|| "unable to get server configuration")?;
@@ -84,8 +130,8 @@ pub fn get_resource(args: &GetArgs) -> Result<()> {
         ResourceTypes::Ethernet => todo!(),
         ResourceTypes::Rooms => get_rooms(&server, &args.resource_id, &args.wide),
         ResourceTypes::Room => get_rooms(&server, &args.resource_id, &args.wide),
-        ResourceTypes::Programs => todo!(),
-        ResourceTypes::Program => todo!(),
+        ResourceTypes::Programs => get_programs(&server, &args.resource_id, &args.wide),
+        ResourceTypes::Program => get_programs(&server, &args.resource_id, &args.wide),
     }
 }
 
@@ -143,3 +189,66 @@ fn get_rooms(server: &Server, room_id: &Option<String>, wide: &bool) -> Result<(
 
     Ok(())
 }
+
+fn get_programs(server: &Server, room_id: &Option<String>, wide: &bool) -> Result<()> {
+  let url = match room_id {
+      Some(id) => format!("{}{}{}/{}", server.url, DEFAULT_PATH, "ProgramLibrary", id),
+      None => format!("{}{}{}", server.url, DEFAULT_PATH, "ProgramLibrary"),
+  };
+
+  let client = reqwest::blocking::Client::new();
+
+  let res = client
+      .get(url)
+      .header(AUTHORIZATION, server.token.as_str())
+      .header(ACCEPT, "application/json")
+      .send()
+      .with_context(|| "Error occured while sending request")?;
+
+  let response: Response = match res.json() {
+      Ok(response) => response,
+      Err(error) => return Err(anyhow::anyhow!("Unable to deserialize response; {}", error)),
+  };
+
+  let programs: HashMap<u32, Program> =
+      match response.device.programs.program_library {
+          Some(instances) => instances,
+          None => HashMap::new(),
+      };
+
+  let program_vec: Vec<Program> = programs.values().cloned().collect();
+
+  if !wide {
+    let table = tabled::Table::new(program_vec)
+          .with(Style::blank())
+          .with(Disable::column(ByColumnName::new("program_id")))
+            .with(Disable::column(ByColumnName::new("notes")))
+            .with(Disable::column(ByColumnName::new("mobility_file")))
+            .with(Disable::column(ByColumnName::new("mobility_file_ts")))
+            .with(Disable::column(ByColumnName::new("web_xpanel_file")))
+            .with(Disable::column(ByColumnName::new("web_xpanel_file_ts")))
+            .with(Disable::column(ByColumnName::new("project_file")))
+            .with(Disable::column(ByColumnName::new("project_file_ts")))
+            .with(Disable::column(ByColumnName::new("cws_file")))
+            .with(Disable::column(ByColumnName::new("cws_file_ts")))
+            .with(Disable::column(ByColumnName::new("compile_date_time")))
+            .with(Disable::column(ByColumnName::new("cres_db_version")))
+            .with(Disable::column(ByColumnName::new("device_db_version")))
+            .with(Disable::column(ByColumnName::new("include_dat_version")))
+            .with(Disable::column(ByColumnName::new("app_file_ts")))
+          .to_string();
+      
+
+      print!("{}", table);
+  } else {
+    let table = tabled::Table::new(program_vec)
+    .with(Style::blank())          
+    .to_string();
+
+      print!("{}", table);
+  }
+
+  Ok(())
+}
+
+

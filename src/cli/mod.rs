be9c69373dfdbfc7717c7fbfce1906bb5f7e6373
Andrew Welker <awelker@pepperdash.com>
2022-11-26 17:34:20 -0700

    refactor: reorganize modules and rename files

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,26 @@
-pub mod cli;
+pub mod config;
+pub mod get;
+
+use clap::{Parser, Subcommand};
+use clap_verbosity_flag::{self, Verbosity};
+use config::Config;
+
+use get::GetArgs;
+
+#[derive(Parser, Debug)]
+#[command(author, version)]
+#[command(about = "vc4ctl - a CLI for interacting with Crestron VC-4 servers")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,    
+
+    #[command(flatten)]
+    pub verbose: Verbosity
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Configure server information
+    Config(Config),
+    Get(GetArgs)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,69 @@
 mod api;
 mod cli;
 
-use api::config;
+use api::config::{add_server, remove_server, update_server, use_server, get_servers};
 use clap::Parser;
-use cli::cli::{Cli, Commands, ConfigCommands};
+use cli::{Cli, Commands};
+use cli::config::{ConfigCommands};
+use tracing::metadata::LevelFilter;
+use tracing::{Level, Subscriber};
+use tracing_subscriber::FmtSubscriber;
+use log;
 
-fn main() -> anyhow::Result<()> {
+fn configure_tracing(args:&Cli) -> impl Subscriber {
+    let builder = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .with_target(false)
+        .without_time();
+
+    match args.verbose.log_level_filter() {
+        log::LevelFilter::Debug => builder.with_max_level(LevelFilter::DEBUG),
+        log::LevelFilter::Error => builder.with_max_level(LevelFilter::ERROR),
+        log::LevelFilter::Info => builder.with_max_level(LevelFilter::INFO),
+        log::LevelFilter::Off => builder.with_max_level(LevelFilter::OFF),
+        log::LevelFilter::Trace => builder.with_max_level(LevelFilter::TRACE),
+        log::LevelFilter::Warn => builder.with_max_level(LevelFilter::WARN)
+    }.finish()
+}
+
+fn main() -> anyhow::Result<()> {  
     let cli = Cli::parse();
+
+    let subscriber = configure_tracing(&cli);
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     match &cli.command {
         Some(Commands::Config(args)) => match &args.config_commands {
-            Some(ConfigCommands::Add(args)) => match config::config::add_server(args.clone()) {
+            Some(ConfigCommands::Add(args)) => match add_server(args.clone()) {
                 Ok(()) => {
                     println!("Server {} successfully added", args.name);
                     return Ok(());
                 }
                 Err(error) => return Err(anyhow::anyhow!(error)),
             },
-            Some(ConfigCommands::Remove(args)) => match config::config::remove_server(args) {
+            Some(ConfigCommands::Remove(args)) => match remove_server(args) {
                 Ok(()) => {
                     println!("Server {} successfully removed", args.name);
                     return Ok(());
                 }
                 Err(error) => return Err(anyhow::anyhow!(error)),
             },
-            Some(ConfigCommands::Update(args)) => match config::config::update_server(args) {
+            Some(ConfigCommands::Update(args)) => match update_server(args) {
                 Ok(()) => {
                     println!("Server {} successfully updated", args.name);
                     return Ok(());
                 }
                 Err(error) => return Err(anyhow::anyhow!(error)),
             },
-            Some(ConfigCommands::Use(args)) => match config::config::use_server(args.clone()) {
+            Some(ConfigCommands::Use(args)) => match use_server(args.clone()) {
                 Ok(()) => {
                     println!("Using Server {}", args.name);
                     return Ok(());
                 }
                 Err(error) => return Err(anyhow::anyhow!(error)),
             },
-            Some(ConfigCommands::GetServers(_args)) => match config::config::get_servers() {
+            Some(ConfigCommands::GetServers(_args)) => match get_servers() {
                 Ok(()) => {
                     return Ok(());
                 }
